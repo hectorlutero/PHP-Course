81a63b09b4d2e354cd205848d7322e7e3540ac63
Hector Siman <61216453+hectorlutero@users.noreply.github.com>
2021-01-24 11:28:49 -0300

    Lecture 20 - Ternaries

---
+++ Section-2_ITS-BROWSER-TIME/20.ternary/index.php
@@ -0,0 +1,11 @@
+<?php
+
+$myName = "Annie";
+
+echo $myName === "Hector" ? "Hello, Hector" : "Hello, some dude";
+
+// if ($myName === "Hector") {
+//     echo "Hello, Hector";
+// } else {
+//     echo "Hello, some dude";
+// }
