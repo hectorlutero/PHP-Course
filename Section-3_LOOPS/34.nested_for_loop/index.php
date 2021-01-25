8d8fb811836f30276de27d6bfbcfc887dad6c5bc
Hector Siman <61216453+hectorlutero@users.noreply.github.com>
2021-01-25 20:09:37 -0300

    34. Lecture - Nested For Loop

---
+++ Section-3_LOOPS/34.nested_for_loop/index.php
@@ -0,0 +1,20 @@
+<?php
+
+// for ($i = 0; $i < 10; $i++) {
+    
+//     for ($j = 0; $j < 10; $j++){
+//         var_dump($i . ' - ' . $j);
+//         echo "<br>";
+//     }
+// }
+
+for ($i = 0; $i < 10; $i++) {
+    
+    for ($i = 0; $i < 10; $i++){
+        var_dump($i);
+        echo "<br>";
+    }
+    var_dump($i);
+    echo "<br>";
+}
+var_dump($i);
