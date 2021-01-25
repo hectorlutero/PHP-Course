f750b607d7c4f28dc94a3389a16ef4f30f517b3b
Hector Siman <61216453+hectorlutero@users.noreply.github.com>
2021-01-25 17:06:37 -0300

    33. Lecture - I++, J++, C++

---
+++ Section-3_LOOPS/33.I++_J++_C++/index.php
@@ -0,0 +1,35 @@
+<?php
+
+// $i = 0;
+// $i++;
+// var_dump($i);
+
+// $i = 32;
+// var_dump($i++);
+// var_dump($i);
+
+// $i = 32;
+// var_dump(++$i);
+// echo "<br>";
+
+// for ($hector = 0; $hector < 10; $hector++) {
+//     var_dump($hector);
+//     echo "<br>";
+// }
+
+// $i = 0;
+// var_dump($i--);
+// var_dump($i);
+
+// $i = 0;
+// var_dump(--$i);
+
+// $i = 0;
+// var_dump(+-$i); 
+
+// $i = 0;
+// var_dump(-+$i); 
+
+// $i = 0
+// var_dump($i-+);  // THIS RETURNS AN ERROR
+
