4c59c97c6539d28d579fc65a06aa629f859b6a06
Hector Siman <61216453+hectorlutero@users.noreply.github.com>
2021-01-25 10:19:38 -0300

    27. Lecture - Using $_COOKIE to store the random number

---
+++ Section-2_ITS-BROWSER-TIME/27.using-$_cookie-to-store-the-number/index.php
@@ -0,0 +1,38 @@
+<?php
+
+$cookieSetter = setcookie('randomNumber', rand(1, 10), time()+3600);
+if (empty($_COOKIE['randomNumber'])) {
+    // $_COOKIE['randomNumber'] = "27" // THAT WORKS BUT IT ISN'T THE BEST WAY TO DO IT
+    var_dump("Setting the cookie");    
+    $cookieSetter;
+}
+
+if (isset($_POST['guess'])) {
+
+    $randomNumber = (int) $_COOKIE['randomNumber'];
+    $number = $_POST['number'];
+
+    if ($randomNumber == $number) {
+        echo "You guessed it";
+        $cookieSetter;
+    } else {
+        echo "Try again, the number was " . $randomNumber;
+        $cookieSetter;
+    }
+} else {
+    echo "nothing to see here";
+}
+
+?>
+
+<html lang="en">
+    <body>
+
+        <form method="POST">
+            <label for="number">Guess a number between 1 and 10.</label>
+            <input type="number" name="number">
+            <input type="submit" value="Guess" name="guess">
+        </form>
+    </body>
+</html>
+
