e4307715bc32a7dddd044f5ff570f4b071bf5bac
Hector Siman <61216453+hectorlutero@users.noreply.github.com>
2021-01-25 22:29:08 -0300

    39. Lecture - Do While Loop

---
+++ Section-3_LOOPS/39.do_while_loop/index.php
@@ -0,0 +1,13 @@
+<?php
+
+$i = 0;
+// while ($i < 10) {
+//     var_dump($i);
+//     echo "<br>";
+//     $i++;
+// }
+
+do {
+    var_dump($i);
+    $i++;
+} while ($i < 0);   // THE DIFFERENCE IS THAT THIS ONE WILL RUN AT LEAST ONE TIME
